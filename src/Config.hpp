a36e007966564a9935ac7b80850a3d9cd634f998
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-26 12:09:29 -0500

    add Config Grid and Snake

--- src/Config.hpp
+++ src/Config.hpp
@@ -8,4 +8,14 @@ namespace Config
   inline constexpr int WINDOW_WIDTH = 600;
   inline constexpr int WINDOW_HEIGHT = 600;
   inline constexpr int FPS_LIMIT = 60;
+
+  // ===== Game =====
+  inline constexpr int GRID_SIZE = 20;
+  inline constexpr int GRID_WIDTH = 30;
+  inline constexpr int GRID_HEIGHT = 30;
+
+  // ===== Snake =====
+  inline constexpr int START_X = 10;
+  inline constexpr int START_Y = 10;
+  inline constexpr float SNAKE_SPEED = 0.1f;
 }
