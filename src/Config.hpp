3716aa46adcfd9b185bef1bddd99531b5c41bf7a
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-26 11:09:55 -0500

    add Config

---
+++ src/Config.hpp
@@ -0,0 +1,11 @@
+// ===== Config.hpp =====
+
+#pragma once
+
+namespace Config
+{
+  // ===== Window =====
+  inline constexpr int WINDOW_WIDTH = 600;
+  inline constexpr int WINDOW_HEIGHT = 600;
+  inline constexpr int FPS_LIMIT = 60;
+}
